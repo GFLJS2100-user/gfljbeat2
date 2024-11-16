82ac159e48322f635a8bf8a1a2007c4dcca2175b
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-11-16 19:27:15 +0800

    Update audioProcessor.mjs

--- scripts/audioProcessor.mjs
+++ scripts/audioProcessor.mjs
@@ -383,7 +383,7 @@ class audioProcessor extends AudioWorkletProcessor {
 					this.getValuesVisualizer = (funcValue) => (funcValue & funcValue >> 12 & 255);
 					break;
 				case 'Trianglebeat':
-					this.getValues = (funcValue) => (((funcValue<<1)^-(funcValue>>7&1))& 255) / 127.5 - 1;
+					this.getValues = (funcValue) => (((funcValue<<1)^-(funcValue>>7&1)) & 255) / 127.5 - 1;
 					this.getValuesVisualizer = (funcValue) => ((funcValue<<1)^-(funcValue>>7&1) & 255);
 					break;
 				case 't/256*(Byte&beat>>12)':
