e39e9649ba7dd368f068ca48d4c9896638e50fbe
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-12-04 20:51:29 +0800

    Update bytebeat.mjs

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -840,7 +840,7 @@ globalThis.bytebeat = new class {
 		this.setCounterValue(this.byteSample);
 	}
 	setCodeSize(value) {
-		this.controlCodeSize.textContent = `${this.formatBytes(new Blob([value]).size)} (${new Blob([this.formatBytes(String(window.location))])})`;
+		this.controlCodeSize.textContent = `${this.formatBytes(new Blob([value]).size)} (${new Blob([String(this.formatBytes((window.location))])})`;
 	}
 	setCounterValue(value) {
 		this.controlTime.value = this.settings.isSeconds ?
